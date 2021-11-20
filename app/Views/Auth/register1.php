4f30461fc53d24b91e570a04c14b44a9596df754
enggar abin <abinplanet@gmail.com>
2021-11-20 05:58:46 -0800

    perbaikan code 20 nov

--- app/Views/Auth/register1.php
+++ app/Views/Auth/register1.php
@@ -3,60 +3,8 @@
 <?= $this->section("Main-body");?>
 <!-- Tampilan untuk form daftar -->
 <div class="register-wrapper">
-    <div class="register-list">
-        <div class="osis-img">
-            <img src="<?= base_url("Img/osis1.png");?>" alt="osis image" class="osis-image" id="osis-image">
-        </div>
-
-        <!-- teks masuk -->
-        <span class="text-register">buat akun baru</span>
-        
-        <!-- form daftar -->
-        <div class="list-create-account">
-        <a href="<?= base_url("Auth/register");?>" class="Thislink register-list-form">data diri</a>
-            <a href="<?= base_url("Auth/register1");?>" class="Thislink register-list-form-1">data ekstrakurikuler</a>
-            <a href="<?= base_url("Auth/register2");?>" class="Thislink register-list-form">tahun masuk</a>
-            <a href="<?= base_url("Auth/register3");?>" class="Thislink register-list-form">jurusan</a>
-            <a href="<?= base_url("Auth/register4");?>" class="Thislink register-list-form">keamanan</a>
-            <a href="<?= base_url("Auth/register5");?>" class="Thislink register-list-form">syarat dan ketentuan</a>
-        </div>
-
-        <a href="" class="Thislink register-btn">sudah punya akun!</a>
-
-        <!-- teks masuk melalui -->
-        <span class="text-register-with">melalui :</span>
-
-        <!-- masuk dengan sosial media -->
-        <div class="register-with">
-            <a href="" class="Thislink register-with-icon rounded-circle google"><i class="fab fa-fw fa-google"></i></a>
-            <a href="" class="Thislink register-with-icon rounded-circle facebook"><i class="fab fa-fw fa-facebook"></i></a>
-            <a href="" class="Thislink register-with-icon rounded-circle instagram"><i class="fab fa-fw fa-instagram"></i></a>
-            <a href="" class="Thislink register-with-icon rounded-circle linkedin"><i class="fab fa-fw fa-linkedin"></i></a>
-            <a href="" class="Thislink register-with-icon rounded-circle github"><i class="fab fa-fw fa-github"></i></a>
-        </div>
-
-        <!-- informasi website -->
-        <div class="bottom-information">
-            <!-- sosial media OSIS -->
-            <div class="social-media-icon">
-                <a href="" class="Thislink sosmed-icon"><i class="fab fa-fw fa-instagram"></i></a>
-                <a href="" class="Thislink sosmed-icon"><i class="fab fa-fw fa-youtube"></i></a>
-                <a href="" class="Thislink sosmed-icon"><i class="fab fa-fw fa-twitter"></i></a>
-                <a href="" class="Thislink sosmed-icon"><i class="fab fa-fw fa-github"></i></a>
-            </div>
-
-            <!-- hak cipta -->
-            <span class="hakcipta text-capitalize" id="hakcipta">
-                OSIS17KABTA
-            </span>
-
-            <!-- kode versi website -->
-            <a href="<?= base_url("Page/version");?>" class="Thislink version" id="version">
-                V.14.2.21
-            </a>
-
-        </div>
-    </div>
+    <!-- kode halaman navigasi register -->
+    <?= $this->include("Layout/nav-register");?>
 
     <!-- formulir pendaftaran -->
     <div class="register-form">
--- app/Views/Home/index.php
+++ app/Views/Home/index.php
@@ -1,6 +1,7 @@
 <?= $this->extend("Layout/index.php");?>
 
 <?= $this->section("Main-body");?>
+
 <!-- kode halaman navigasi -->
 <?= $this->include("Layout/nav");?>
 
@@ -11,108 +12,8 @@
 <main class="main-content">
     <!-- kontent utama -->
     <div class="left-content">
-        <!-- status teman -->
-        <div class="status-friends">
-            <!-- slider status teman -->
-            <div class="slider-status">
-                <a href="" class="Thislink s-friend-left"><i class="rounded-circle fa fa-fw fa-chevron-left"></i></a>
-                <a href="" class="Thislink s-friend-right"><i class="rounded-circle fa fa-fw fa-chevron-right"></i></a>
-            </div>
-
-            <!-- status konten -->
-            <div class="status-friend-content">
-                <!-- status -->
-                <div class="status-wrapper rounded-circle">
-                    <div class="friend-img rounded-circle">
-                        <img src="<?= base_url("Img/u4.jpg");?>" alt="" class="user-status-img">
-                    </div>
-                </div>
-                
-                <!-- status -->
-                <div class="status-wrapper rounded-circle">
-                    <div class="friend-img rounded-circle">
-                        <img src="<?= base_url("Img/u1.jpg");?>" alt="" class="user-status-img">
-                    </div>
-                </div>
-
-                <!-- status -->
-                <div class="status-wrapper rounded-circle">
-                    <div class="friend-img rounded-circle">
-                        <img src="<?= base_url("Img/u2.jpg");?>" alt="" class="user-status-img">
-                    </div>
-                </div>
-                
-                <!-- status -->
-                <div class="status-wrapper rounded-circle">
-                    <div class="friend-img rounded-circle">
-                        <img src="<?= base_url("Img/u3.jpg");?>" alt="" class="user-status-img">
-                    </div>
-                </div>
-
-                <!-- status -->
-                <div class="status-wrapper rounded-circle">
-                    <div class="friend-img rounded-circle">
-                        <img src="<?= base_url("Img/u5.jpg");?>" alt="" class="user-status-img">
-                    </div>
-                </div>
-
-                <!-- status -->
-                <div class="status-wrapper rounded-circle">
-                    <div class="friend-img rounded-circle">
-                        <img src="<?= base_url("Img/u6.jpg");?>" alt="" class="user-status-img">
-                    </div>
-                </div>
-
-                <!-- status -->
-                <div class="status-wrapper rounded-circle">
-                    <div class="friend-img rounded-circle">
-                        <img src="<?= base_url("Img/u7.jpg");?>" alt="" class="user-status-img">
-                    </div>
-                </div>
-
-                <!-- status -->
-                <div class="status-wrapper rounded-circle">
-                    <div class="friend-img rounded-circle">
-                        <img src="<?= base_url("Img/u8.jpg");?>" alt="" class="user-status-img">
-                    </div>
-                </div>
-                
-                <!-- status -->
-                <div class="status-wrapper rounded-circle">
-                    <div class="friend-img rounded-circle">
-                        <img src="<?= base_url("Img/u9.jpg");?>" alt="" class="user-status-img">
-                    </div>
-                </div>
-
-                <!-- status -->
-                <div class="status-wrapper rounded-circle">
-                    <div class="friend-img rounded-circle">
-                        <img src="<?= base_url("Img/u10.jpg");?>" alt="" class="user-status-img">
-                    </div>
-                </div>
-
-                <!-- status -->
-                <div class="status-wrapper rounded-circle">
-                    <div class="friend-img rounded-circle">
-                        <img src="<?= base_url("Img/u11.jpg");?>" alt="" class="user-status-img">
-                    </div>
-                </div>
-
-                <!-- status -->
-                <div class="status-wrapper rounded-circle">
-                    <div class="friend-img rounded-circle">
-                        <img src="<?= base_url("Img/u12.jpg");?>" alt="" class="user-status-img">
-                    </div>
-                </div>
-
-                <!-- status -->
-                <div class="status-wrapper rounded-circle">
-                    <div class="friend-img rounded-circle">
-                        <img src="<?= base_url("Img/u13.jpg");?>" alt="" class="user-status-img">
-                    </div>
-                </div>
-            </div>
-        </div>
+        <!-- kode halaman status friend -->
+        <?= $this->include("Layout/status-friend");?>
 
         <!-- konten utama informasi sekolah -->
         <div class="main-information-content rounded">
