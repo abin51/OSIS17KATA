227618baf9c48d9d5dd5b02f3bdb686f0c3683a9
enggar abin <abinplanet@gmail.com>
2021-12-18 20:58:21 +0700

    update code sebelum bikin auth

--- app/Views/Auth/index.php
+++ app/Views/Auth/index.php
@@ -77,81 +77,12 @@
 
     <!-- bagian informasi mengenai sekolah -->
     <div class="school-information">
-        <!-- logo -->
-        <div class="logo">
-            <!-- logo -->
-            <img src="<?= base_url("Img/paskib.png")?>" alt="logo img" class="logo-img" id="logo-img">
-
-            <!-- teks logo -->
-            <div class="text-logo text-uppercase">
-                <span class="name-img">pasukan pengibar bendera pusaka</span>
-                <span class="name-school">sma negeri 17 kabupaten tangerang</span>
-            </div>
-        </div>
-
-        <!-- halaman utama informasi sekolah -->
         <div class="information">
-            <!-- paragraf untuk menjelaskan gambar -->
-            <div class="par-image"></div>
-
             <!-- background sekolah, organisasi dan ekstrakurikuller -->
             <div class="bg-image">
                 <img src="<?= base_url("Img/p9.jpg");?>" alt="bg image" class="bg-soe" id="bg-soe">
             </div>
         </div>
-
-        <!-- slider informasi -->
-        <div class="slider-information-wrapper">
-            <div class="slider-image-information">
-                <!-- tombol pindah informasi -->
-                <button class="slide-top-btn Thislink rounded-circle">
-                    <!-- tombol slide atas -->
-                    <i class="fa fa-fw fa-chevron-up"></i>
-                </button>
-                
-                <!-- slide manual -->
-                <div class="slider-btn-manual">
-                    <!-- checkbox disembunyikan -->
-                    <input type="checkbox" name="ex1" id="ex1" class="ex1 check-sekom">
-                    <input type="checkbox" name="ex2" id="ex2" class="ex2 check-sekom">
-                    <input type="checkbox" name="ex3" id="ex3" class="ex3 check-sekom">
-                    <input type="checkbox" name="ex4" id="ex4" class="ex4 check-sekom">
-                    <input type="checkbox" name="ex5" id="ex5" class="ex5 check-sekom">
-                    <input type="checkbox" name="ex6" id="ex6" class="ex6 check-sekom">
-                    <input type="checkbox" name="ex7" id="ex7" class="ex7 check-sekom">
-                    <input type="checkbox" name="ex8" id="ex8" class="ex8 check-sekom">
-                    <input type="checkbox" name="ex9" id="ex9" class="ex9 check-sekom">
-                    <input type="checkbox" name="ex10" id="ex10" class="ex10 check-sekom">
-                    <input type="checkbox" name="ex11" id="ex11" class="ex11 check-sekom">
-                    <input type="checkbox" name="ex12" id="ex12" class="ex12 check-sekom">
-
-                    <!-- checkbox custom -->
-                    <label for="ex1" class="sekom"></label>
-                    <label for="ex2" class="sekom"></label>
-                    <label for="ex3" class="sekom"></label>
-                    <label for="ex4" class="sekom"></label>
-                    <label for="ex5" class="sekom"></label>
-                    <label for="ex6" class="sekom"></label>
-                    <label for="ex7" class="sekom"></label>
-                    <label for="ex8" class="sekom"></label>
-                    <label for="ex9" class="sekom"></label>
-                    <label for="ex10" class="sekom"></label>
-                    <label for="ex11" class="sekom"></label>
-                    <label for="ex12" class="sekom"></label>
-                </div>
-                
-                <button class="slide-down-btn Thislink rounded-circle">
-                    <!-- tombol slide bawah -->
-                    <i class="fa fa-fw fa-chevron-down"></i>
-                </button>
-            </div>
-        </div>
-
-        <!-- teks informasi ekstrakurikuller -->
-        <a href="" class="Thislink text-information">
-            <h1 class="title-information">Lorem ipsum dolor sit amet.</h1>
-            <span class="main-information-excul">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit ducimus commodi inventore dolores adipisci aperiam sit optio, necessitatibus delectus laboriosam.</span>
-        </a>
     </div>
 </div>
 <?= $this->endSection();?>
